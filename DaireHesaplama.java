2ac00c03f841c1ecc7a6851c70aea272f6d4ee4f
Fatmanur Oral <fatmanuroral00@gmail.com>
2023-04-05 17:56:59 +0300

    Daire Alanını Hesaplayan Program

---
+++ DaireHesaplama.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DaireHesaplama {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double r, mAci, alan;
+
+        System.out.print("yarciap degerini giriniz :");
+        r = inp.nextDouble();
+
+        System.out.print("merkez aci degerini giriniz :");
+        mAci = inp.nextDouble();
+
+        alan = (Math.PI * (r * r) * mAci) / 360;
+        System.out.print("Daire diliminin alani : " + alan);
+    }
+}
