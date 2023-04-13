56ea0ff4c9bf88901aa2f362d2bc09e11332584f
Fatmanur00 <66698363+Fatmanur00@users.noreply.github.com>
2023-04-13 17:31:23 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double armut=2.14, elma=3.67,domates=1.11,muz=0.95,patlıcan=5,toplam,a,e,d,m,p;
+        int n1,n2,select;
         Scanner inp=new Scanner(System.in);
-        System.out.println("Armut kaç kilo giriniz:");
-        a=inp.nextDouble();
-        a=a*armut;
-        System.out.println("Elma kaç kilo giriniz:");
-        e=inp.nextDouble();
-        e=e*elma;
-        System.out.println("Domates kaç kilo giriniz:");
-        d=inp.nextDouble();
-        d=d*domates;
-        System.out.println("Muz kaç kilo giriniz:");
-        m=inp.nextDouble();
-        m=m*muz;
-        System.out.println("Patlıcan kaç kilo giriniz:");
-        p=inp.nextDouble();
-        p=p*patlıcan;
-
-        toplam=a+e+d+m+p;
-        System.out.println("Toplam tutarınız:"+toplam);
+        System.out.println("1.Sayıyı Giriniz:");
+        n1=inp.nextInt();
+        System.out.println("2.Sayıyı Giriniz:");
+        n2=inp.nextInt();
+        System.out.println("1-Toplama 2-Çıkarma 3-Çarpma 4-Bölme" +
+                "Birini Seçiniz:");
+        select= inp.nextInt();
+        switch (select) {
+            case 1:
+                System.out.println("Toplam"+ (n1+n2));
+                break;
+            case 2:
+                System.out.println("Çıkarma"+ (n1-n2));
+                break;
+            case 3:
+                System.out.println("Çarpma"+ (n1*n2));
+                break;
+            case 4:
+                System.out.println("Bölme"+ (n1/n2));
+                break;
+            default:
+                System.out.println("Sayı 1,2,3,4 değildir.");
+                break;
+        }
     }
 }
