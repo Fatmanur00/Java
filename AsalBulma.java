1cf33e80630feda0c751f62163f4777049aa028c
Fatmanur Oral <Fatmanuroral00@gmail.com>
2023-06-22 17:05:48 +0300

    Add files via upload

---
+++ AsalBulma.java
@@ -0,0 +1,18 @@
+
+public class AsalBulma {
+    public static void main(String[] args) {
+
+        for (int i=2 ; i<=100 ; i++){
+            int n =0;
+            for(int j =2 ; j <=100 ; j++){
+                if (i%j == 0){
+
+                    n++;
+                }
+            }
+            if (n<2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
